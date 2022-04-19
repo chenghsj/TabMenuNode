b0a4b6bfeb395a63d914c4e06d6f58efa2c10be1
Cheng <chengjohnsonhs@gmail.com>
2022-04-19 12:19:40 +0800

    move click and hold to new branch

--- inject/inject.js
+++ inject/inject.js
@@ -96,37 +96,3 @@ window.onmousedown = async function (e) {
  * TODO: other pages tab
  * TODO: group tab
  */
-
-// window.onmousedown = async function (e) {
-// 	let { clientWidth, clientHeight } = GetWindowSize();
-// 	let tabList, isTabMenu, isFunctionalNode;
-// 	if (e.x > clientWidth || e.y > clientHeight) return;
-// 	try {
-// 		tabList = await getTabList();
-// 		isTabMenu = await module({ fnName: "isTabMenu", node: e.target });
-// 		isFunctionalNode = await module({ fnName: "isFunctionalNode", node: e.target });
-// 	} catch (err) {
-// 		console.error(err);
-// 	}
-// 	// console.log(tabList);
-// 	if (!isTabMenu && tabMenu?.visibility) {
-// 		clearTimeout(timeout_id);
-// 		tabMenu.visible(false);
-// 		return;
-// 	}
-// 	if (e.button == 0 && !isFunctionalNode) {
-// 		timeout_id = setTimeout(async function () {
-// 			tabMenu.addList(tabList);
-// 			tabMenu.setPosition(e, { clientWidth, clientHeight });
-// 			tabMenu.visible(true);
-// 		}, 250);
-// 	}
-// };
-// window.onmouseup = function () {
-// 	if (timeout_id) clearTimeout(timeout_id);
-// };
-// window.onmousemove = function (e) {
-// 	if (e.movementX <= 0.1 && e.movementX >= -0.1) return;
-// 	else if (e.movementY <= 0.1 && e.movementY >= -0.1) return;
-// 	if (timeout_id) clearTimeout(timeout_id);
-// };
