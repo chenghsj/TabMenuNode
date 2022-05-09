4247fada04ee9ef9e5e5f0aaa4dcd82615280c7e
chenghsj <43364792+chenghsj@users.noreply.github.com>
2022-05-09 15:14:07 +0800

    fix click and hold bug

--- inject/event/clickAndHoldRight.js
+++ inject/event/clickAndHoldRight.js
@@ -1,5 +1,6 @@
 function ClickAndHoldRight(args) {
 	let timeout_id;
+	let contextmenuOpened = false;
 	let { module, GetWindowSize, getAllTabList, tabMenu, time_interval, getAllStorageSyncData } =
 		args;
 	window.onmousedown = async function (e) {
@@ -11,39 +12,56 @@ function ClickAndHoldRight(args) {
 		if (!isTabMenu && tabMenu?.visibility) {
 			clearTimeout(timeout_id);
 			tabMenu.visible(false);
+			contextmenuOpened = false;
 			return;
 		}
 		//right click for window system
 		if (e.button === 2) {
 			timeout_id = setTimeout(function () {
-				tabMenu.setStorageData(storageData).then(() => {
-					if (storageData.showOtherWindows) {
-						tabMenu.addList(tabList[0], tabList[1]);
-					} else {
-						tabMenu.addList(tabList[0]);
-					}
-					tabMenu.setPosition(e, { clientWidth, clientHeight });
-					tabMenu.visible(true);
-				});
+				if(!contextmenuOpened) {
+					tabMenu.setStorageData(storageData).then(() => {
+						if (storageData.showOtherWindows) {
+							tabMenu.addList(tabList[0], tabList[1]);
+						} else {
+							tabMenu.addList(tabList[0]);
+						}
+						tabMenu.setPosition(e, { clientWidth, clientHeight });
+						tabMenu.visible(true);
+					});
+				}
+				
 			}, time_interval);
 		}
 	};
 	window.addEventListener("contextmenu", function (e) {
+		contextmenuOpened = true;
 		if (timeout_id) clearTimeout(timeout_id);
 		// window system's contextmenu is triggered by keyup;
 		if (tabMenu?.visibility) {
+			contextmenuOpened = false;
 			e.preventDefault();
 		}
 	});
 
 	window.onmouseup = function () {
 		if (timeout_id) clearTimeout(timeout_id);
+		contextmenuOpened = false;
 	};
 
 	window.onmousemove = function (e) {
 		if (e.movementX <= 0.1 && e.movementX >= -0.1) return;
 		else if (e.movementY <= 0.1 && e.movementY >= -0.1) return;
-		if (timeout_id) clearTimeout(timeout_id);
+		if (timeout_id) {
+			clearTimeout(timeout_id);
+			contextmenuOpened = false;
+		};
+	};
+
+	window.onkeyup = function (e) {
+		if (e.key === "Escape") {
+			tabMenu.visible(false);
+			contextmenuOpened = false;
+		}
 	};
 }
 
